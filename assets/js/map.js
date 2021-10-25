3b0303a57b3787a8d6240a13b4c0f2edfc0dbc6c
Maxime-code <74910872+MaximePETIT-code@users.noreply.github.com>
2021-10-25 09:48:44 +0200

    first-commit

---
+++ assets/js/map.js
@@ -0,0 +1,152 @@
+function initMap() {
+    
+    const adress = { lat: 48.9749152, lng: 2.37762 };
+
+    
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 16,
+      center: adress,
+      styles: [
+      {
+          "featureType": "all",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "color": "#202c3e"
+              }
+          ]
+      },
+      {
+          "featureType": "all",
+          "elementType": "labels.text.fill",
+          "stylers": [
+              {
+                  "gamma": 0.01
+              },
+              {
+                  "lightness": 20
+              },
+              {
+                  "weight": "1.39"
+              },
+              {
+                  "color": "#ffffff"
+              }
+          ]
+      },
+      {
+          "featureType": "all",
+          "elementType": "labels.text.stroke",
+          "stylers": [
+              {
+                  "weight": "0.96"
+              },
+              {
+                  "saturation": "9"
+              },
+              {
+                  "visibility": "on"
+              },
+              {
+                  "color": "#000000"
+              }
+          ]
+      },
+      {
+          "featureType": "all",
+          "elementType": "labels.icon",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "landscape",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "lightness": 30
+              },
+              {
+                  "saturation": "9"
+              },
+              {
+                  "color": "#1b1b1b"
+              }
+          ]
+      },
+      {
+          "featureType": "poi",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "saturation": 20
+              }
+          ]
+      },
+      {
+          "featureType": "poi.park",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "lightness": 20
+              },
+              {
+                  "saturation": -20
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "lightness": 10
+              },
+              {
+                  "saturation": -30
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "geometry.fill",
+          "stylers": [
+              {
+                  "color": "#193a55"
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "geometry.stroke",
+          "stylers": [
+              {
+                  "saturation": 25
+              },
+              {
+                  "lightness": 25
+              },
+              {
+                  "weight": "0.01"
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "lightness": -20
+              }
+          ]
+      }
+  ]
+    });
+    
+    const marker = new google.maps.Marker({
+      position: adress,
+      map: map,
+    });
+  }
